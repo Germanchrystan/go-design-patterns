32e8f03627c1e2ec2775bbc2ecd05b11cfe19e84
unknown <germanchrystan@gmail.com>
2022-04-22 17:00:11 -0300

    Command first example

--- Design_Patterns/13_Command/A)_Command/command.go
+++ Design_Patterns/13_Command/A)_Command/command.go
@@ -1,1 +1,73 @@
 package main
+
+import "fmt"
+
+/*
+	We are going to construct a bank account struct, with withdrawal and deposit operations.
+*/
+var overdraftlimit = -500
+
+type BankAccount struct {
+	balance int
+}
+
+func (b *BankAccount) Deposit(amount int) {
+	b.balance += amount
+	fmt.Println("Deposited", amount, "\b, balance is now", b.balance)
+}
+
+func (b *BankAccount) Withdraw(amount int) {
+	if b.balance-amount >= overdraftlimit {
+		b.balance -= amount
+		fmt.Println("Withdrew", amount, "\b, balance is now", b.balance)
+	}
+}
+
+/*
+	Now we can set up the command pattern. There are
+	different ways of handling commands.
+
+	One way is where the bank account itself handles the command.
+	The other is when the command handles itself. We are going to consider this scenario.
+*/
+
+type Command interface {
+	Call()
+}
+
+type Action int
+
+const (
+	Deposit Action = iota
+	Withdraw
+)
+
+type BankAccountCommand struct {
+	account *BankAccount
+	action  Action
+	amount  int
+}
+
+func (b BankAccountCommand) Call() {
+	switch b.action {
+	case Deposit:
+		b.account.Deposit(b.amount)
+	case Withdraw:
+		b.account.Withdraw(b.amount)
+	}
+}
+
+// We can also make a factory function for this command struct
+func NewBankAccountCommand(account *BankAccount, action Action, amount int) *BankAccountCommand {
+	return &BankAccountCommand{account: account, action: action, amount: amount}
+}
+
+func main() {
+	ba := BankAccount{}
+	cmd := NewBankAccountCommand(&ba, Deposit, 100)
+	cmd.Call()
+	fmt.Println(ba)
+	cmd2 := NewBankAccountCommand(&ba, Withdraw, 50)
+	cmd2.Call()
+	fmt.Println(ba)
+}
---
+++ Design_Patterns/13_Command/README.md
@@ -0,0 +1,12 @@
+# Command Design Pattern
+When we write ordinary code statements, this statements are perishable. Suppose you do a field assignement, for example, yo assign the field age to the value of 20. You cannot by default undo that operation because there is no record of it. You also cannot serialize the sequence of actions or calls.
+What we want is an object that represents an operation. For example, you might have a command which says that a construct 'person' should change its age to value 22. In this case, you would:
+- Define a structure where you would say what construct has to change (person).
+- What needs to be changed in this construct (age).
+- What value should be changed to (22). 
+
+Tipically, any kind of user interface is done using commands. Commands also allow us to do multilevel undo and redo, by saving every command that was sent to some system, and then allowing for those commands to roll back and forth.
+We can also group commands together and save a whole sequence of commands. These are typically called macros in GUI applications.
+
+A Command is an object which represents an instruction to perform a particular action. Contains all the information necessary for the action to be taken. 
+A command can be processed by the object that the command operates on, or processed by itself. It can basically have a call method to apply its commands or alternatively, there could also be a command processor, where all the commands are sent.
